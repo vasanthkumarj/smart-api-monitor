210e7f0ea3226dfc8f35cc3c5201d3462c6566b6
vasanthkumar <vasanthkumar.j@outlook.com>
2025-10-11 16:35:49 +0530

    Initial commit

---
+++ src/main/java/com/example/smart_api_monitor/SmartApiMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_api_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartApiMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartApiMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_api_monitor/SmartApiMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_api_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartApiMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
